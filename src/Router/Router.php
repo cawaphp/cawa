dbb59ec3aa6d6a51ad2a75b7f7eb3376b610b7a5
tchiotludo <tchiot.ludo@gmail.com>
2016-06-07 17:15:07 +0200

    Revert "Router : call_user_func_array to $callable()"
    This reverts commit a0d5b86ba8e95decb08f2d18270f59a0da5571d6.

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -603,7 +603,7 @@ class Router
 
         // simple function
         if (is_callable($callback) && is_object($callback)) {
-            return $callback(...$args);
+            return call_user_func_array($callback, [$args]);
         }
 
         // controller
@@ -646,10 +646,10 @@ class Router
         ]);
 
         if (method_exists($controller, 'init')) {
-            ([$controller, 'init'])(...$ordererArgs);
+            call_user_func_array([$controller, 'init'], $ordererArgs);
         }
 
-        $return = ([$controller, $method])(...array_values($ordererArgs));
+        $return = call_user_func_array([$controller, $method], $ordererArgs);
 
         self::dispatcher()->emit($event);
 
