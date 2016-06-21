1fdf14e772c4cca53656f128cfcc86df9516278a
tchiotludo <tchiot.ludo@gmail.com>
2016-06-21 09:50:01 +0200

    VarDumper : config for expandedLevel

--- src/VarDumper/HtmlDumper.php
+++ src/VarDumper/HtmlDumper.php
@@ -13,6 +13,8 @@ declare (strict_types=1);
 
 namespace Cawa\VarDumper;
 
+use Cawa\Core\DI;
+
 class HtmlDumper extends \Symfony\Component\VarDumper\Dumper\HtmlDumper
 {
     use DumperTrait;
@@ -34,9 +36,38 @@ class HtmlDumper extends \Symfony\Component\VarDumper\Dumper\HtmlDumper
         );
 
         $header = parent::getDumpHeader();
+
+        $header = str_replace(
+            "function toggle(a, recursive) {",
+
+            "function getLevel(elt) {\n" .
+            "    var exit = false, level = 0, element = elt;\n" .
+            "\n" .
+            "    if (element.parentNode.className == 'sf-dump') {\n" .
+            "        return 0;\n" .
+            "    }\n" .
+            "\n" .
+            "    while(exit == false) {\n" .
+            "        element = element.parentNode;\n" .
+            "        level++;\n" .
+            "        exit = !element.parentNode || element.parentNode.className == 'sf-dump';\n" .
+            "    }\n".
+
+            "    return level;\n" .
+            "}\n" .
+            "\n" .
+            "function toggle(a, recursive) {",
+            $header
+        );
+
+        $expandLevel = DI::config()->getIfExists('varDumper/expandLevel');
+        if (is_null($expandLevel)) {
+            $expandLevel = 4;
+        }
+
         $header = preg_replace(
             "/if \\('sf-dump' != elt.parentNode.className\\) \\{.*\\}/mU",
-            '',
+            $expandLevel == false ? '' : "if (getLevel(elt) > " . $expandLevel . ") {toggle(a);}",
             $header,
             -1,
             $count
