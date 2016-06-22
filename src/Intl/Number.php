0be18cb44bf8a35d43a9522e1ba7ad104e30783f
tchiotludo <tchiot.ludo@gmail.com>
2016-06-22 17:48:22 +0200

    Intl Number Formatter

---
+++ src/Intl/Number.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace Cawa\Intl;
+
+use NumberFormatter;
+
+class Number
+{
+    use TranslatorFactory;
+
+    /**
+     * @param float $value
+     * @param string $currency
+     *
+     * @return string
+     */
+    public static function formatCurrency(float $value, string $currency) : string
+    {
+        $formatter = new NumberFormatter(self::translator()->getIETF(), NumberFormatter::CURRENCY);
+        return $formatter->formatCurrency($value, $currency);
+    }
+}
