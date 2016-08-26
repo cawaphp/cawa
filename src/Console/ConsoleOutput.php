100708cf51712e1907556a845138641812c77c8e
tchiotludo <tchiot.ludo@gmail.com>
2016-08-26 15:34:03 +0200

    ConsoleOutput : add a timer

--- src/Console/ConsoleOutput.php
+++ src/Console/ConsoleOutput.php
@@ -43,6 +43,11 @@ class ConsoleOutput extends BaseConsoleOutput implements ConsoleOutputInterface
     }
 
     /**
+     * @var float
+     */
+    private $previous;
+
+    /**
      * Add timestamp/duration to given string
      *
      * @param string $message
@@ -51,11 +56,19 @@ class ConsoleOutput extends BaseConsoleOutput implements ConsoleOutputInterface
      */
     public function prefixWithTimestamp($message)
     {
+        $diff = $this->previous ? round((microtime(true) - $this->previous) , 3) : 0;
+        $this->previous = microtime(true);
+
         $microtime = explode(' ', (string) microtime())[0];
         $microtime = substr((string) round($microtime, 3), 2, 3);
         $microtime = str_pad($microtime, 3, '0');
 
-        $prefix = sprintf('<fg=white>[%s.%s]</>', date('Y-m-d H:i:s'), $microtime);
+        $prefix = sprintf(
+            '<fg=white>[%s.%s]</> <fg=yellow>[+%s s]</>',
+            date('Y-m-d H:i:s'),
+            $microtime,
+            str_pad((string) $diff, 9, " ", STR_PAD_LEFT)
+        );
         $prefix = $this->getFormatter()->format($prefix);
 
         $message = str_pad($prefix, strlen($prefix) + 1) . $message;
